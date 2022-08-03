b29582b49ad46894909576334e2ec2f1ca91449e
João Vitor Marra <marrajoaosa@gmail.com>
2022-08-03 11:18:56 -0300

    Slider finished

--- assets/elements/icons/ArrowRight.js
+++ assets/elements/icons/ArrowRight.js
@@ -1,15 +1,18 @@
-export function ArrowRight({color}) {
+export function ArrowRight() {
     return (
-        <svg
+      <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="100%"
+      width="33"
+      height="33"
       fill="none"
-      viewBox="0 0 20 8"
+      viewBox="0 0 33 33"
     >
       <path
-        fill={color}
-        d="M19.354 4.354a.5.5 0 000-.708L16.172.464a.5.5 0 10-.708.708L18.293 4l-2.829 2.828a.5.5 0 10.708.708l3.182-3.182zM0 4.5h19v-1H0v1z"
+        stroke="#000"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="3"
+        d="M5.156 16.5h22.688M18.563 7.219l9.28 9.281-9.28 9.281"
       ></path>
     </svg>
     )
