fbb67270242f1d7668d97c7277be1cf4e7f22b9a
João Vitor Marra <marrajoaosa@gmail.com>
2022-08-08 19:16:40 -0300

    About us (Company's page)

--- components/MainComponents/MainEmpresa/AboutDescription/index.js
+++ components/MainComponents/MainEmpresa/AboutDescription/index.js
@@ -1,22 +1,22 @@
 export function AboutDescription() {
     return (
-        <section >
-            
-            <div className="absolute layout-padding -top-16 lg:-top-28">
-                <div className="bg-gray-500 px-6 py-3 rounded-lg">
-                    <h3 className="font-bold text-4xl h-16 lg:h-28">
+        <section className="relative w-full xl:w-3/5">
+            <div className="layout-padding -translate-y-16 lg:-translate-y-28">
+                <div className="bg-gray-500 px-6 py-3 rounded-lg relative lg:static lg:py-6">
+                    <h3 className="font-bold text-4xl h-16 md:text-5xl lg:h-24">
                         Sobre nós
                     </h3>
-                    <p className="text-xs leading-6">
-                        O Grupo Smart Solutions é uma empresa especializada em Serviços, locação de equipamentos e transportes desde 2006.
-                        Na Gestão da Cadeia de Suprimentos atuamos nos transportes Dedicados, Spot, Inbound, Outbound, Cross Docking, Distribuição, Armazenagem , Armazém Geral e movimentaçao de carga.
-                        O Grupo Smart Solutions trabalha para que seus clientes se tornen mais competitivos , atraves de ganhos de produtividade, melhores soluçoes nos processos, reduçoes de custo globais e foco em suas atividades totalmente voltada para segurança nas operaçoes. 
-                        Para isso a empresa conta com equipamentos moveis de última geraçao e profissionais capacitados, focado sempre em segurança e eficiencia garantindo assim a satisfaçao do cliente.
-                        Operando em todo Território Nacional, Matriz na capital Mineira e pontos de apoio nos demais Estados da Federação, em conformidade com as estruturas de nossos parceiros.
+                    <p className="text-xs leading-6 pb-14 md:text-xl">
+                    O Grupo Smart Solutions é uma empresa especializada em Serviços, locação de equipamentos e transportes desde 2006.
+                    Na Gestão da Cadeia de Suprimentos atuamos nos transportes Dedicados, Spot, Inbound, Outbound, Cross Docking, Distribuição, Armazenagem , Armazém Geral e movimentaçao de carga.
+                    O Grupo Smart Solutions trabalha para que seus clientes se tornen mais competitivos , atraves de ganhos de produtividade, melhores soluçoes nos processos, reduçoes de custo globais e foco em suas atividades totalmente voltada para segurança nas operaçoes. 
+                    Para isso a empresa conta com equipamentos moveis de última geraçao e profissionais capacitados, focado sempre em segurança e eficiencia garantindo assim a satisfaçao do cliente.
+                    Operando em todo Território Nacional, Matriz na capital Mineira e pontos de apoio nos demais Estados da Federação, em conformidade com as estruturas de nossos parceiros.
                     </p>
+                    <div className="bg-blue-500 w-[100px] h-[100px] rounded-full absolute right-0 -translate-y-1/2 lg:w-44 lg:h-44"></div>
                 </div>
             </div>
-            <div className="w-full bg-yellow-500 pt-56"></div>
+            <div className="w-full h-20 bg-yellow-500 absolute top-1/4 -z-10"></div>
         </section>
     )
 }
