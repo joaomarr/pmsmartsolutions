54a5e64d2a17a8f49e5db6e4413564e9a8b1cf41
João Vitor Marra <marrajoaosa@gmail.com>
2022-08-13 00:54:26 -0300

    Disable auto dark-mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import {Layout} from '../components/Layout'
-import Head from "next/Head"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
   return (
